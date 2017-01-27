62a9e50b68207e7e5fb21d6df703f79291337403
Rodion Zinovyev <rodionz@012.net.il>
2017-01-27 09:31:42 +0200

    service imported to components

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,3 +1,4 @@
+import { DataserviceService } from '../dataservice.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataservice: DataserviceService) { }
 
   ngOnInit() {
   }
--- src/app/kupotholim/kupotholim.component.ts
+++ src/app/kupotholim/kupotholim.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataserviceService } from '../dataservice.service';
 
 @Component({
   selector: 'app-kupotholim',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KupotholimComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataservice: DataserviceService) { }
 
   ngOnInit() {
   }
